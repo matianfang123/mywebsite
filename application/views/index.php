6e7c37f709849207b4eeb1874739778821b0c120
2246437666@qq.com <qq2246437666>
2016-10-08 23:47:36 +0800

    header 基本样式

---
+++ application/views/index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>我的小网页</title>
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <base href="<?php echo site_url();?>">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
+    <script type="text/javascript"> addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        } </script>
+</head>
+<body>
+<header id="header">
+    <section id="header-left">
+        <img src="img/1.jpg" alt="">
+        <h1>马天放</h1>
+        <p>web engineer</p>
+    </section>
+    <section id="header-right">
+        <h1>contact</h1>
+        <p>email:2246437666@qq.com</p>
+        <p>TEL:+18845076353</p>
+    </section>
+</header>
+<section id="about-me">
+</section>
+<section id="my-skills"></section>
+<section id="resume"></section>
+<div id="analyze"></div>
+<div id="lightspot"></div>
+<div id="my-photo"></div>
+<div id="contact"></div>
+<footer></footer>
+
+<script src="js/jquery.js"></script>
+<script>
+    $(function(){
+
+    });
+</script>
+</body>
+</html>
